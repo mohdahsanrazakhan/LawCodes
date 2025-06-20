6d377186003e5a559fb53dbbd64f7e072a313b15
Mark <datawithmark@gmail.com>
2025-06-20 23:46:37 +0530

    update domain name LegalCodes

--- src/pages/BlogPosts.jsx
+++ src/pages/BlogPosts.jsx
@@ -50,52 +50,61 @@ const BlogPosts = () => {
 
   return (
     <div className="max-w-4xl mx-auto px-4 py-6 space-y-6">
-      {posts.map(post => (
-        <div key={post.id} className="flex sm:flex-row flex-col border rounded p-2 gap-2 sm:gap-4 bg-slate-50 dark:bg-[#222831] hover:shadow-sm">
-          <img
-            src={post.images?.[0]?.url || post.content.match(/<img.*?src="(.*?)"/)?.[1] || "/default-thumbnail.jpg"}
-            // src={extractThumbnail(post.content) || "/default-thumb.jpg"}
-            alt="thumbnail"
-            loading="lazy"
-            className="sm:w-50 w-auto h-auto object-cover rounded"
-          />
-          <div className="flex flex-col gap-2">
-            <h2 className="text-xl font-semibold text-gray-800 dark:text-[#c6bdff]">{post.title}</h2>
-            <p className="text-gray-600 dark:text-gray-400 text-sm">
-              {new Date(post.published).toLocaleDateString()} by {post.author?.displayName}
-            </p>
-            {/* <p dangerouslySetInnerHTML={{ __html: post.content.slice(0, 150) + "..." }}></p> */}
-            <p className="text-gray-600 dark:text-gray-200 text-sm">
-              {extractShortDescription(post.content)}
-            </p>
-            <button className="text-white bg-[#5c47c4] px-3 py-1 rounded sm:w-fit w-auto">
-              <Link to={`/post/${post.id}`} >Read More</Link>
-            </button>
-          </div>
+      <h2 className="text-2xl font-bold text-[#5c47c4] dark:text-[#c6bdff]">Blogs</h2>
+      {!posts || posts.length === 0 ? (
+        <div className="text-gray-500 dark:text-gray-200">
+          No blog found.
         </div>
-      ))}
+      ) : (
+        <>
+          {posts.map(post => (
+            <div key={post.id} className="flex sm:flex-row flex-col border rounded p-2 gap-2 sm:gap-4 bg-slate-50 dark:bg-[#222831] hover:shadow-sm">
+              <img
+                src={post.images?.[0]?.url || post.content.match(/<img.*?src="(.*?)"/)?.[1] || "/default-thumbnail.jpg"}
+                // src={extractThumbnail(post.content) || "/default-thumb.jpg"}
+                alt="thumbnail"
+                loading="lazy"
+                className="sm:w-50 w-auto h-auto object-cover rounded"
+              />
+              <div className="flex flex-col gap-2">
+                <h2 className="text-xl font-semibold text-gray-800 dark:text-[#c6bdff]">{post.title}</h2>
+                <p className="text-gray-600 dark:text-gray-400 text-sm">
+                  {new Date(post.published).toLocaleDateString()} by {post.author?.displayName}
+                </p>
+                {/* <p dangerouslySetInnerHTML={{ __html: post.content.slice(0, 150) + "..." }}></p> */}
+                <p className="text-gray-600 dark:text-gray-200 text-sm">
+                  {extractShortDescription(post.content)}
+                </p>
+                <button className="text-white bg-[#5c47c4] px-3 py-1 rounded sm:w-fit w-auto">
+                  <Link to={`/post/${post.id}`} >Read More</Link>
+                </button>
+              </div>
+            </div>
+          ))}
 
-      <div className="flex gap-4 mt-4">
-        {prevTokens.length >= 1 && (
-          <button
-            onClick={() => {
-              const tokens = [...prevTokens];
-              tokens.pop(); // remove current
-              const prevToken = tokens[tokens.length - 1];
-              setPrevTokens(tokens);
-              loadPosts(prevToken);
-            }}
-            className="px-4 py-2 border rounded text-gray-800 dark:text-gray-200"
-          >
-            Previous
-          </button>
-        )}
-        {nextPageToken && (
-          <button onClick={() => loadPosts(nextPageToken)} className="px-4 py-2 border rounded text-gray-800 dark:text-gray-200">
-            Next
-          </button>
-        )}
-      </div>
+          <div className="flex gap-4 mt-4">
+            {prevTokens.length >= 1 && (
+              <button
+                onClick={() => {
+                  const tokens = [...prevTokens];
+                  tokens.pop(); // remove current
+                  const prevToken = tokens[tokens.length - 1];
+                  setPrevTokens(tokens);
+                  loadPosts(prevToken);
+                }}
+                className="px-4 py-2 border rounded text-gray-800 dark:text-gray-200"
+              >
+                Previous
+              </button>
+            )}
+            {nextPageToken && (
+              <button onClick={() => loadPosts(nextPageToken)} className="px-4 py-2 border rounded text-gray-800 dark:text-gray-200">
+                Next
+              </button>
+            )}
+          </div>
+        </>
+      )}
     </div>
   );
 };
